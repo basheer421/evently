40320dbe69748b23437988067cc5cc1946ca956d
basheer421 <basheer123b2@gmail.com>
2025-08-12 04:58:13 +0400

    feat: add text input component and integrate it into the register page

---
+++ resources/views/components/textInput.blade.php
@@ -0,0 +1,8 @@
+<div class="w-full">
+    <input
+        type="{{ $type }}"
+        name="{{ $name }}"
+        placeholder="{{ $placeholder }}"
+        class="bg-gray-100 p-4 rounded-2xl w-full"
+    />
+</div>
--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,6 +1,9 @@
 <x-app title="Register">
     <x-logo />
-    <div class="pt-20 flex justify-center items-center">
+    <div class="pt-20 flex flex-col justify-center items-center">
         <p class="font-bold font-sans text-3xl">Create an account</p>
+        <div class="w-1/3 pt-8">
+            <x-textInput type="text" name="email" placeholder="Email address" />
+        </div>
     </div>
 </x-app>
