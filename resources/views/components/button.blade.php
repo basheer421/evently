24f61be1a048cbb3f7189db2421e2bcc8ee8454d
basheer421 <basheer123b2@gmail.com>
2025-08-12 05:49:04 +0400

    feat: add button and googleButton components, update register page with new options

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+<button
+    class="bg-blue-500 text-white p-4 rounded-3xl w-full hover:bg-blue-600 hover:cursor-pointer transition duration-200 h-12 flex justify-center items-center font-semibold">
+    {{ $slot }}
+</button>
--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -5,5 +5,23 @@
         <div class="w-1/3 pt-8">
             <x-textInput type="text" name="email" placeholder="Email address" />
         </div>
+        <div class="w-1/3 pt-8">
+            <x-button> Continue with email </x-button>
+        </div>
+
+        <p class="pt-8 text-gray-500">Or continue with</p>
+
+        <div class="w-1/3 pt-8">
+            <x-googleButton />
+        </div>
+
+        <p class="pt-8 text-gray-500">Want to be an organizer? <a href="?role=organizer" class="underline">Click
+                here</a>
+        </p>
+
+        <p class="pt-8 text-gray-500">By continuing, you agree to Evently's <a href="terms" class="underline">Terms of
+                Service</a> and acknowledge you've read
+            our <a href="privacy" class="underline">Privacy Policy</a>.</p>
+
     </div>
 </x-app>
